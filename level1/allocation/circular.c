1f4b85ea70580d18fe73bc990a0870661bf58b67
hanichris <hanichris71@gmail.com>
2024-12-20 03:21:04 +0300

    implement missing function definitions. Add pragma to stop reporting of deprecated diagnostic

--- level1/allocation/circular.c
+++ level1/allocation/circular.c
@@ -1,6 +1,7 @@
-#include <stdlib.h>
-
 #include "circular.h"
+#if __GNUC__ > 4 && __GNUC__ <= 14
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
 
 
 [[deprecated("implementation")]]
@@ -48,4 +49,74 @@ double* circular_element(circular const* c, size_t pos) {
 		}
 	}
 	return ret;
+}
+
+[[deprecated("implementation")]]
+circular* circular_append(circular *c, double value) {
+	if (c) {
+		if (c->len < c->cap) {
+			c->tab[c->len] = value;
+			c->len++;
+		}
+	}
+	return c;
+}
+
+[[deprecated("implementation")]]
+size_t circular_getlength(const circular *c) {
+	if (c) {
+		return c->len;
+	}
+	return 0;
+}
+
+[[deprecated("implementation")]]
+double circular_pop(circular *c) {
+	double val = 0.0;
+	if (c && c->len > 0) {
+		val = c->tab[c->start];
+		c->start++;
+		c->len--;
+	}
+	return val;
+}
+
+[[nodiscard("returned pointer replaces function argument.")]]
+[[deprecated("implementation")]]
+circular* circular_resize(circular *c, size_t nlen) {
+	if (c) {
+		size_t len = c->len;
+		if (len > nlen) return nullptr;
+		size_t olen = c->cap;
+		if (nlen != olen) {
+			size_t ostart = circular_getpos(c, 0);
+			size_t nstart = ostart;
+			[[maybe_unused]]double* otab = c->tab;
+			double* ntab = nullptr;
+			if (nlen > olen) {
+				ntab = realloc(c->tab, sizeof(double[nlen]));
+				if (!ntab) return nullptr;
+				if (ostart + len > olen) {
+					size_t ulen = olen - ostart;
+					size_t llen = len - ulen;
+					if (llen <= (nlen - olen)) {
+						memcpy(ntab + olen, ntab, llen * (sizeof(double)));
+					} else {
+						nstart = nlen - ulen;
+						memmove(ntab + nstart, ntab + ostart, ulen * sizeof(double));
+					}
+				}
+
+			}
+
+			*c = (circular) {
+				.cap = nlen,
+				.start = nstart,
+				.len = len,
+				.tab = ntab,
+			};
+		}
+
+	}
+	return c;
 }
